1e07c8f057c88517b4e48b2582123a2bffc57f08
alpin54 <alpinn189@gmail.com>
2025-12-15 21:18:34 +0700

    update

--- src/components/AboutUs/AboutUsOurBranches/views/index.jsx
+++ src/components/AboutUs/AboutUsOurBranches/views/index.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 // -- components
-import IndonesiaMap from '@components/AboutUs/AboutUsOurBranches/views/indonesiaMap';
+// import IndonesiaMap from '@components/AboutUs/AboutUsOurBranches/views/indonesiaMap';
 
 // -- styles
 import style from '@components/AboutUs/AboutUsOurBranches/styles/style.module.scss';
@@ -13,7 +13,7 @@ const AboutUsOurBranches = (props) => {
     <section className={style.ourBranches}>
       <div className='container'>
         <h2 className={style.title}>{data.title}</h2>
-        <IndonesiaMap geoUrl={data?.geoUrl} markers={data?.branches} />
+        {/* <IndonesiaMap geoUrl={data?.geoUrl} markers={data?.branches} /> */}
       </div>
     </section>
   );
